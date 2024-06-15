f3ca655c3b7276a51307a5732f9d6c4ef6c16308
Recxo <rishava065@gmail.com>
2024-06-15 23:15:19 +0530

    made some changes in app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,redirect,url_for,request
 import joblib
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -12,6 +14,21 @@ def index():
     return render_template('index.html',**locals())
 
 
+user_predicted_values = { 'sepal_length':[],
+                         'sepal_width':[],
+                         'petal_length':[],
+                         'petal_width':[],
+                         'species':[]}
+
+
+# creating the dataframe for storing later.
+user_data = pd.DataFrame(columns=['sepal_length',
+                         'sepal_width',
+                         'petal_length',
+                         'petal_width',
+                         'species'])
+
+
 @app.route('/predict',methods=['POST','GET'])
 def submit():
     if request.method == 'POST':
@@ -20,8 +37,25 @@ def submit():
         petal_length = float(request.form['petal_length'])
         petal_width = float(request.form['petal_width'])
         prediction = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])[0]
-        history = {''}
 
+        user_predicted_values['sepal_length'].append(sepal_length)
+        user_predicted_values['sepal_width'].append(sepal_width)
+        user_predicted_values['petal_length'].append(petal_length)
+        user_predicted_values['petal_width'].append(petal_length)
+        user_predicted_values['species'].append(prediction)
+
+        user_predicted_dataframe = pd.DataFrame(user_predicted_values)
+        pd.concat([user_data,user_predicted_dataframe],axis=1)
+        print(user_data)
+
+
+
+
+        
+        
+        
+ 
+    # using **locals() instead of dictionary for context and sending it the frontend is sometimes hectic. It saves the time and automatically sees and send all the data to index.html file
 
     return render_template('index.html',**locals())
 
